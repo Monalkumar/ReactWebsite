ea624f1e839cbf566509635116f622a8d8f0bbd6
MonalKumar <monalshiv123@gmail.com>
2021-02-18 14:03:34 -1100

    my website

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 const Footer =()=>{
     return(
         <footer class="container-fluid text-center">
-  <a href="#" title="To Top">
+  <a href="#myPag" title="To Top">
     <span class="glyphicon glyphicon-chevron-up"></span>
   </a>
   <p>Bootstrap Theme Made By <a href="https://www.w3schools.com" title="Visit w3schools">Monal.kumar</a></p>
