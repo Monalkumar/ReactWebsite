4fe0a6f4626a565039060914fab5b23d4ff1f4c3
MonalKumar <monalshiv123@gmail.com>
2021-02-18 14:34:33 -1100

    Updates

---
+++ precache-manifest.cd2652e128bc3071d4cdd45f055b64fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb5297e07c9eed6dc40d536cf3e3236a",
+    "url": "/ReactWebsite/index.html"
+  },
+  {
+    "revision": "d6ac20c6e5f72cb6608d",
+    "url": "/ReactWebsite/static/css/main.fca36cb2.chunk.css"
+  },
+  {
+    "revision": "f5cb13d5fc33e94126e3",
+    "url": "/ReactWebsite/static/js/2.f85388ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactWebsite/static/js/2.f85388ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6ac20c6e5f72cb6608d",
+    "url": "/ReactWebsite/static/js/main.c6a4f03e.chunk.js"
+  },
+  {
+    "revision": "6d59fd568002c73cc189",
+    "url": "/ReactWebsite/static/js/runtime-main.9c858a19.js"
+  }
+]);
